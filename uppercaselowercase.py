c4e67c5b5145d86a8b6217a12b597e8fa9ac1f87
jyotichauhan20 <72217749+jyotichauhan20@users.noreply.github.com>
2021-08-11 19:41:15 +0530

    Add files via upload

---
+++ uppercaselowercase.py
@@ -0,0 +1,14 @@
+def countstring(string):
+    case={"upper":0, "lower":0}
+    for j in string:
+        if j.isupper():
+           case["upper"]+=1
+        elif j.islower():
+           case["lower"]+=1
+        else:
+           continue
+    print (string)
+    print ("Upper", case["upper"])
+    print ("Lower", case["lower"])
+
+countstring("My Name is KItTy")
